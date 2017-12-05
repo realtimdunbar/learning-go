93dffa1ceafb5c982d414a1ff91052b7d8046539
Tim Dunbar <tim.dunbar@gmail.com>
2017-12-04 20:46:08 -0500

    chapter 5 complete

---
+++ golang-book/chapter5/exercise_4/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+
+	var lowest int = 100
+
+	for _, value := range x {
+		if value < lowest {
+			lowest = value
+		}
+	}
+
+	fmt.Println(lowest)
+}
